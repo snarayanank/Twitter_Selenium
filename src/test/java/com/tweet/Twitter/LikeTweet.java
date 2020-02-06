f9afc7156d40f8b59a5fe98c2ae9947ff6d4e485
Sathiya Narayanan Kalyanasundaram <56080883+snarayanank@users.noreply.github.com>
2020-02-07 00:02:20 +0800

    Update LikeTweet.java

--- src/test/java/com/tweet/Twitter/LikeTweet.java
+++ src/test/java/com/tweet/Twitter/LikeTweet.java
@@ -35,7 +35,7 @@ public class LikeTweet extends Base{
 		WebDriverWait wait = new WebDriverWait(driver,20);
 		wait.until(ExpectedConditions.visibilityOf(loginPage.get_LoginBtn()));
 		wait.until(ExpectedConditions.titleContains("Twitter"));
-		loginPage.loginTwitter(prop.getProperty("validUser"), prop.getProperty("validUser"));
+		loginPage.loginTwitter(prop.getProperty("validUser"), prop.getProperty("validPwd"));
 		HomePage homePage = new HomePage(driver);
 		wait.until(ExpectedConditions.visibilityOf(homePage.getTweetBox()));
 		homePage.searchUser("@snarayanank");
